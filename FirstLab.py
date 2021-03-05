44d3a0b34b30eb5474f53f998a81fe0456217ae6
Dmitry Mukhachev <dmitry.mukhachev@netcracker.com>
2021-03-05 18:11:45 +0300

    Added Fibonacci generator logic

--- FirstLab.py
+++ FirstLab.py
@@ -18,19 +18,38 @@ class Fibonacci:
     t = 0
     x_arr = []
 
-    def __init__(self, r=None, s=None, x_arr=None):
+    def __init__(self, r=None, s=None, x_arr=None, max_num=128):
         if r is None:
-            self.r = randint(0, 128)
+           r = randint(0, max_num)
+        self.r = r
         if s is None:
-            self.s = randint(0, 128)
+            s = randint(0, max_num)
+            while s > r:
+                s = randint(0, max_num)
+        self.s = s
         if x_arr is None:
-            self.x_arr = []
+            x_arr = []
             for i in range(r):
-                self.x_arr.append(randint(0, 128))
-        self.t = r
+                x_arr.append(randint(0, max_num))
+        self.x_arr = x_arr
+        assert r == len(self.x_arr), 'r is not equal to arr len'
+        self.t = r - 1
+        print('Fibonacci instance created! r = {}, s = {}, x_arr = {}'.format(self.r, self.s, self.x_arr))
 
     def get_new_x(self):
         res = self.x_arr[self.t - self.r] ^ self.x_arr[self.t - self.s]
         self.x_arr.append(res)
         self.t += 1
-        return
+        return res
+
+    def write_x_res_to_file(self, num, file_name):
+        with open(file_name, "w") as f:
+            for i in range(num):
+                res = self.get_new_x()
+                f.write('{}\n'.format(res))
+
+
+
+
+
+
